1c30a23fe0799868f8db3f102f988b5981bcef32
garritrj <garritrj@miamioh.edu>
2024-03-13 10:38:28 -0400

    Created constructors for cheese object

--- cheese.cpp
+++ cheese.cpp
@@ -18,4 +18,12 @@ class Cheese {
         string countryOfOrigin;
         // Image address for a picture of the cheese (may change)
         string imageAddress;
+
+    public:
+        // Constructors for cheese object
+        Cheese(): name("Cheese"), price(0.0),
+        countryOfOrigin("USA"), imageAddress("") {}
+        Cheese(string chName, double chPrice, string chOrigin, string chImageAddress) :
+        name(chName), price(chPrice), countryOfOrigin(chOrigin), imageAddress(chImageAddress) {}
+
 };
