aded8dc0e93387bd18ea53ace97dd2fd79b64287
garritrj <garritrj@miamioh.edu>
2024-03-13 10:31:59 -0400

    Creating private variables for cheese object

--- cheese.cpp
+++ cheese.cpp
@@ -5,3 +5,17 @@
 #include <string>
 
 using namespace std;
+
+// Cheese class
+class Cheese {
+    // Variables for cheese object
+    private:
+        // Name of the cheese
+        string name;
+        // Price of the cheese
+        double price;
+        // Country of origin (may change)
+        string countryOfOrigin;
+        // Image address for a picture of the cheese (may change)
+        string imageAddress;
+};
